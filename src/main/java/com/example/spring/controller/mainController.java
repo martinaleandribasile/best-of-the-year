d266c864f6a7940629a286803dc4c92f3a9cf4e2
MartinaBasile <martinaleandribasile@gmail.com>
2023-02-13 16:36:59 +0100

    add style, bootstrap, dettails page

--- src/main/java/com/example/spring/controller/mainController.java
+++ src/main/java/com/example/spring/controller/mainController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.spring.model.Movie;
@@ -25,10 +26,16 @@ public class mainController {
 	
 	private List<Movie> getBestMovie(){
 		List<Movie> moviesList = new ArrayList<>(); 
-		Movie movie1= new Movie(1, "Film1");
-		Movie movie2= new Movie(2, "Film2");
+		Movie movie1= new Movie(1, "il Ciclone");
+		Movie movie2= new Movie(2, "Die Hard");
+		Movie movie3= new Movie(3, "Kill Bill vol1");
+		Movie movie4= new Movie(4, "Kill Bill vol2");
+		Movie movie5= new Movie(5, "Il Padrino");
 		moviesList.add(movie1);
 		moviesList.add(movie2);
+		moviesList.add(movie3);
+		moviesList.add(movie4);
+		moviesList.add(movie5);
 		return moviesList;
 		
 	}
@@ -53,6 +60,20 @@ public class mainController {
 		List<Song> songs= getBestSong();
 		model.addAttribute("canzoni" , songs);
 		return "songs";
+	}
+	
+	@GetMapping("/songs/{id}") 	
+	public String dettaglioCanzone(Model model, @PathVariable("id") String id ) {
+		int idSong= Integer.parseInt(id)-1;
+		model.addAttribute("song", getBestSong().get(idSong));
+		return "canzone";
+	}
+	
+	@GetMapping("/movies/{id}") 	
+	public String dettaglioFilm(Model model, @PathVariable("id") String id ) {
+		int idFilm= Integer.parseInt(id)-1;
+		model.addAttribute("film", getBestMovie().get(idFilm));
+		return "film";
 	}
 	
 }
